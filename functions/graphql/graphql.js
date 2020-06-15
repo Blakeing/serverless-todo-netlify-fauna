80af566af808e03dfdb78c4b1be8a0b7b908caa9
Blake Ingenthron <blakeingenthron@gmail.com>
2020-06-15 16:14:14 -0500

    Create graphql.js

---
+++ functions/graphql/graphql.js
@@ -0,0 +1,18 @@
+const { ApolloServer, gql } = require('apollo-server-lambda');
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: { hello: () => 'Hello world!' },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: true,
+  introspection: true,
+});
