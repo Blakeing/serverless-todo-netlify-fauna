e8efa8eb0c0c8756be3221d1e5a9bd438f5cbd52
Blake Ingenthron <blakeingenthron@gmail.com>
2020-06-15 18:45:03 -0500

    updated graphQL server

--- functions/graphql/index.js
+++ functions/graphql/index.js
@@ -1,4 +1,8 @@
 const { ApolloServer, gql } = require('apollo-server-lambda');
+const faunadb = require('faunadb');
+const q = faunadb.Query;
+
+var client = new faunadb.Client({ secret: process.env.Fauna });
 
 const typeDefs = gql`
   type Query {
@@ -15,25 +19,61 @@ const typeDefs = gql`
   }
 `;
 
-const todos = {};
-let todoIndex = 0;
+//could have errors
 
 const resolvers = {
   Query: {
-    todos: () => {
-      return Object.values(todos);
+    todos: async (parent, args, { user }) => {
+      if (!user) {
+        return [];
+      } else {
+        const results = await client.query(
+          q.Paginate(q.Match(q.Index('todos_by_user'), 'user-test'))
+        );
+        return results.data.map(([ref, text, done]) => ({
+          id: ref.id,
+          text,
+          done,
+        }));
+      }
     },
   },
   Mutation: {
-    addTodo: (_, { text }) => {
-      todoIndex++;
-      const id = `key-${todoIndex}`;
-      todos[id] = { id, text, done: false };
-      return todos[id];
+    addTodo: async (_, { text }, { user }) => {
+      if (!user) {
+        throw new Error('Must be authenticated to insert todos');
+      }
+      const results = await client.query(
+        q.Create(q.Collection('todos'), {
+          data: {
+            text,
+            done: false,
+            owner: user,
+          },
+        })
+      );
+      return {
+        ...results.data,
+        id: results.ref.id,
+      };
     },
-    updateTodoDone: (_, { id }) => {
-      todos[id].done = true;
-      return todos[id];
+    updateTodoDone: async (_, { id }) => {
+      if (!user) {
+        throw new Error('Must be authenticated to insert todos');
+      }
+      const results = await client.query(
+        q.Update(
+          q.Ref(q.Ref(q.Collection('todos'), id), {
+            data: {
+              done: true,
+            },
+          })
+        )
+      );
+      return {
+        ...results.data,
+        id: results.ref.id,
+      };
     },
   },
 };
