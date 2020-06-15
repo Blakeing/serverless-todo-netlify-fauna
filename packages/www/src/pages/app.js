f5dac26c963f0ca57a02c599e764182cb1325117
Blake Ingenthron <blakeingenthron@gmail.com>
2020-06-15 16:03:52 -0500

    Bunch of stuff

--- packages/www/src/pages/app.js
+++ packages/www/src/pages/app.js
@@ -1,25 +1,14 @@
 import React, { useContext } from 'react';
-import { Container, Heading, Button, Flex, NavLink } from 'theme-ui';
-import { Link } from 'gatsby';
+import { Router, Link } from '@reach/router';
+import { Container, Flex, Heading, Button, NavLink } from 'theme-ui';
 import { IdentityContext } from '../../identity-context';
-export default (props) => {
+import Dash from '../components/dashboard';
+
+let DashLoggedOut = (props) => {
   const { user, identity: netlifyIdentity } = useContext(IdentityContext);
 
   return (
     <Container>
-      <Flex as="nav">
-        <NavLink as={Link} to="/" p={2}>
-          Home
-        </NavLink>
-        <NavLink as={Link} to={'/app'} p={2}>
-          Dashboard
-        </NavLink>
-        {user && (
-          <NavLink href="#!" p={2}>
-            {user.user_metadata.full_name}
-          </NavLink>
-        )}
-      </Flex>
       <Flex sx={{ flexDirection: 'column', padding: 3 }}>
         <Heading as="h1">Get Stuff Done</Heading>
         <Button
@@ -32,5 +21,22 @@ export default (props) => {
         </Button>
       </Flex>
     </Container>
+  );
+};
+
+export default (props) => {
+  const { user } = useContext(IdentityContext);
+
+  if (!user) {
+    return (
+      <Router>
+        <DashLoggedOut path="/app" />
+      </Router>
+    );
+  }
+  return (
+    <Router>
+      <Dash path="/app" />
+    </Router>
   );
 };
