f0d7a30c14d94b84bbc5f6e0c01e251b19b9c4c4
Blake Ingenthron <blakeingenthron@gmail.com>
2020-06-16 16:59:50 -0500

    moved signup outside graphql folder

---
+++ functions/identity-signup.js
@@ -0,0 +1,46 @@
+const fetch = require('node-fetch');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (event) => {
+  const { user } = JSON.parse(event.body);
+
+  const customer = await stripe.customers.create({ email: user.email });
+
+  await stripe.subscriptions.create({
+    customer: customer.id,
+    items: [{ plan: 'price_HJGIUO7C5Zxl2O' }],
+  });
+
+  const netlifyID = user.id;
+  const stripeID = customer.id;
+
+  const response = await fetch('https://graphql.fauna.com/graphql', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${process.env.FAUNA_SERVER_KEY}`,
+    },
+    body: JSON.stringify({
+      query: `
+          mutation ($netlifyID: ID! $stripeID: ID!) {
+              createUser(data: {netlifyID: $netlifyID, stripeID: $stripeID}) {
+                  netlifyID
+                  stripeID
+              }
+          }
+          `,
+      variables: {
+        netlifyID,
+        stripeID,
+      },
+    }),
+  })
+    .then((res) => res.json())
+    .catch((err) => console.error(JSON.stringify(err, null, 2)));
+
+  console.log({ response });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ app_metadata: { roles: ['sub:free'] } }),
+  };
+};
