9b81eb71c3036811af18984074c65f5acccf25b1
Blake Ingenthron <blakeingenthron@gmail.com>
2020-06-15 23:01:47 -0500

    Update gatsby-browser.js

--- packages/www/gatsby-browser.js
+++ packages/www/gatsby-browser.js
@@ -23,7 +23,8 @@ const authLink = setContext((_, { headers }) => {
 });
 
 const httpLink = new HttpLink({
-  uri: '/.netlify/functions/graphql',
+  uri:
+    'https://blakes-serverless-todo-netlify-fauna.netlify.app/.netlify/functions/graphql',
 });
 
 const client = new ApolloClient({
